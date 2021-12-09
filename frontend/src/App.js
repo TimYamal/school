92a0c9305e72adf65002e7459ecfa979f2292887
Nikita Pavlov <73684551+Bzigg@users.noreply.github.com>
2021-12-09 22:36:40 +0500

    настроил состояние, подключил axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Index from './components/Index';
@@ -8,14 +8,31 @@ import List from './components/List';
 import Teacher from './components/Teacher';
 
 function App() {
+
+  const [statePage, setPage] = useState('index');
+
+  function openPage (newPage) {
+    setPage(newPage);
+  }
+
   return (
     <div className="App wrapper">
-      <Header />
-      {/* <Index /> */}
-      {/* <Course /> */}
-      {/* <About /> */}
-      {/* <List /> */}
-      <Teacher />
+      <Header 
+      updatePage={openPage}/>
+
+      { (() => {
+          if (statePage === 'index') {
+            return <Index />
+          } else if (statePage === 'courses') {
+            return <Course />
+          } else if (statePage === 'teachers') {
+            return <Teacher />
+          } else if (statePage === 'about') {
+            return <About />
+          }
+        })()
+      }
+
       <Footer />
     </div>
   );
