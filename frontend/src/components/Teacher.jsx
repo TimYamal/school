a61ce27e1b3906cfe126b77db9b5f40b94937f00
Nikita Pavlov <73684551+Bzigg@users.noreply.github.com>
2021-12-10 19:58:27 +0500

    подключил страницу-компонент с преподавателями

--- frontend/src/components/Teacher.jsx
+++ frontend/src/components/Teacher.jsx
@@ -2,13 +2,41 @@ import React from 'react';
 
 function Teacher() {
   return (
-    <main class="course container">
-        <div className="list">
-            Список преподавателей
+    <main class="teachers">
+      <div class="teachers_Container">
+        <div class="teachers_item">
+          <img class="teacher_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBTn8OO8ODvYLZsVsuGvl9dNswWuulv9bspg&usqp=CAU" alt="" />
+          <p class="teacher_name">Иванов Иван Иванович</p>
+          <p class="teacher_course">Frontend</p>
+          <p class="teacher_disc">Я Работаю на должности такой-то. В компаниии Такой-то  5 лет.Мне очень нравится</p>
         </div>
-        <div className="list">
-            Список преподавателей
+        <div class="teachers_item">
+          <img class="teacher_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBTn8OO8ODvYLZsVsuGvl9dNswWuulv9bspg&usqp=CAU" alt="" />
+          <p class="teacher_name">Иванов Иван Иванович</p>
+          <p class="teacher_course">Frontend</p>
+          <p class="teacher_disc">Я Работаю на должности такой-то. В компаниии Такой-то  5 лет.Мне очень нравится</p>
         </div>
+        <div class="teachers_item">
+          <img class="teacher_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBTn8OO8ODvYLZsVsuGvl9dNswWuulv9bspg&usqp=CAU" alt="" />
+          <p class="teacher_name">Иванов Иван Иванович</p>
+          <p class="teacher_course">Frontend</p>
+          <p class="teacher_disc">Я Работаю на должности такой-то. В компаниии Такой-то  5 лет.Мне очень нравится</p>
+        </div>
+      </div>
+      <div class="teachers_Container">
+        <div class="teachers_item">
+          <img class="teacher_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBTn8OO8ODvYLZsVsuGvl9dNswWuulv9bspg&usqp=CAU" alt="" />
+          <p class="teacher_name">Иванов Иван Иванович</p>
+          <p class="teacher_course">Frontend</p>
+          <p class="teacher_disc">Я Работаю на должности такой-то. В компаниии Такой-то  5 лет.Мне очень нравится</p>
+        </div>
+        <div class="teachers_item">
+          <img class="teacher_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBTn8OO8ODvYLZsVsuGvl9dNswWuulv9bspg&usqp=CAU" alt="" />
+          <p class="teacher_name">Иванов Иван Иванович</p>
+          <p class="teacher_course">Frontend</p>
+          <p class="teacher_disc">Я Работаю на должности такой-то. В компаниии Такой-то  5 лет.Мне очень нравится</p>
+        </div>
+      </div>
     </main>
   );
 }
