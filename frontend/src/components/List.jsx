cf9a3d26f756023cad69c76ce1815fc62ed2ce77
Nikita Pavlov <73684551+Bzigg@users.noreply.github.com>
2021-12-12 20:41:33 +0500

    добавил список курсов

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -2,18 +2,53 @@ import React from 'react';
 
 function List() {
   return (
-    <main class="course container">
-        <div className="list">
-            Список курсов
-        </div>
-        <div className="list">
-            Список курсов
-        </div>
-        <div className="list">
-            Список курсов
-        </div>
-        <div className="list">
-            Список курсов
+    <main class="lessons container">
+        <div className="lessons__content">
+            <div class="lessons__item">
+                <p class="lessons__title">Frontend</p>
+                <p class="lessons__disc">Я Работаю на должности такой-то. В компаниии Такой-то 5 лет.Мне очень нравится</p>
+                <p class="lessons__date">21 Сентября 11:00</p>
+                <div class="lessons__btns">
+                    <div class="lesson__btn btn-primary">Подробнее</div>
+                    <div class="lesson__btn btn-secondary">Записаться</div>
+                </div>
+            </div>
+            <div class="lessons__item">
+                <p class="lessons__title">Frontend</p>
+                <p class="lessons__disc">Я Работаю на должности такой-то. В компаниии Такой-то 5 лет.Мне очень нравится</p>
+                <p class="lessons__date">21 Сентября 11:00</p>
+                <div class="lessons__btns">
+                    <div class="lesson__btn btn-primary">Подробнее</div>
+                    <div class="lesson__btn btn-secondary">Записаться</div>
+                </div>
+            </div>
+            <div class="lessons__item">
+                <p class="lessons__title">Frontend</p>
+                <p class="lessons__disc">Я Работаю на должности такой-то. В компаниии Такой-то 5 лет.Мне очень нравится</p>
+                <p class="lessons__date">21 Сентября 11:00</p>
+                <div class="lessons__btns">
+                    <div class="lesson__btn btn-primary">Подробнее</div>
+                    <div class="lesson__btn btn-secondary">Записаться</div>
+                </div>
+            </div>
+            <div class="lessons__item">
+                <p class="lessons__title">Frontend</p>
+                <p class="lessons__disc">Я Работаю на должности такой-то. В компаниии Такой-то 5 лет.Мне очень нравится</p>
+                <p class="lessons__date">21 Сентября 11:00</p>
+                <div class="lessons__btns">
+                    <div class="lesson__btn btn-primary">Подробнее</div>
+                    <div class="lesson__btn btn-secondary">Записаться</div>
+                </div>
+            </div>
+            <div class="lessons__item">
+                <p class="lessons__title">Frontend</p>
+                <p class="lessons__disc">Я Работаю на должности такой-то. В компаниии Такой-то 5 лет.Мне очень нравится</p>
+                <p class="lessons__date">21 Сентября 11:00</p>
+                <div class="lessons__btns">
+                    <div class="lesson__btn btn-primary">Подробнее</div>
+                    <div class="lesson__btn btn-secondary">Записаться</div>
+                </div>
+            </div>
         </div>
     </main>
   );
