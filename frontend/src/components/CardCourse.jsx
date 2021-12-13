706467c3e0e56aab008a1e72333bba8fd4ec7105
Nikita Pavlov <73684551+Bzigg@users.noreply.github.com>
2021-12-13 22:45:26 +0500

    CoursesList переписал под get

--- frontend/src/components/CardCourse.jsx
+++ frontend/src/components/CardCourse.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function CardCourse(props) {
     console.log(props.props)
-    const  {id, name, username} = props.props;
+    const  {date_start, description, title} = props.props;
     // const  {id, name, username} = props.props;
 
     return (
         <div class="lessons__item">
-            <p class="lessons__title">{id}</p>
-            <p class="lessons__disc">{name}</p>
-            <p class="lessons__date">{username}</p>
+            <p class="lessons__title">{title}</p>
+            <p class="lessons__disc">{description}</p>
+            <p class="lessons__date">{date_start}</p>
             <div class="lessons__btns">
                 <div class="lesson__btn btn btn-primary">Подробнее</div>
                 <div class="lesson__btn btn btn-secondary">Записаться</div>
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -8,8 +8,8 @@ function List() {
 
     async function fetchCourses () {
     
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-        // const response = await axios.get('http://127.0.0.1:5000/courses/');
+        // const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const response = await axios.get('http://127.0.0.1:5000/courses/');
         setCards(response.data);
     }
     
