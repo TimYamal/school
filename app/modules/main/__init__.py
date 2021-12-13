496fd88fa8a631241cd0cc09da5d69ee07a18d1d
TimYamal <timyamal@yandex.ru>
2021-12-13 20:48:34 +0500

    admin

--- app/modules/main/__init__.py
+++ app/modules/main/__init__.py
@@ -1,12 +1,29 @@
 from flask import Blueprint
+from flask_admin.contrib.sqla import ModelView
 from flask_restful import Api, Resource, reqparse
 
-from app import db
+from app import db, admin
 
-from .models import Course
+from .models import Course, Member, Teacher
 
 bp = Blueprint('main', __name__)
 api = Api(bp)
+
+
+class CourseModelView(ModelView):
+    column_labels = {
+        'title': 'Название',
+        'short_description': 'Краткое описание',
+        'description': 'Описание',
+        'date_start': 'Дата начала',
+        'duration': 'Кол-во занятий',
+        'teacher': 'Преподаватель'
+    }
+
+
+admin.add_view(CourseModelView(Course, db.session, name='Курсы'))
+admin.add_view(ModelView(Teacher, db.session, name='Преподаватели'))
+admin.add_view(ModelView(Member, db.session, name='Участники'))
 
 
 class Courses(Resource):
